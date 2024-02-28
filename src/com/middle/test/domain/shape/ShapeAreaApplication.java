9725aa5890ed16992e60a7018d083c91d5526a15
www-author <dream970215@naver.com>
2024-02-28 16:31:28 +0900

    :recycle: refactor : 객체지향 설계 원칙 DIP를 고려한 상위 요소로 참조 되도록 변경 (업 캐스팅)

--- src/com/middle/test/domain/shape/ShapeAreaApplication.java
+++ src/com/middle/test/domain/shape/ShapeAreaApplication.java
@@ -2,9 +2,9 @@ package com.middle.test.domain.shape;
 
 public class ShapeAreaApplication {
     public static void main(String[] args) {
-        Circle circle = new Circle(5);
-        Rectangle rectangle = new Rectangle(4, 10);
-        Triangle triangle = new Triangle(5, 3.14);
+        Shape circle = new Circle(5);
+        Shape rectangle = new Rectangle(4, 10);
+        Shape triangle = new Triangle(5, 3.14);
 
         circle.getArea();
         rectangle.getArea();
